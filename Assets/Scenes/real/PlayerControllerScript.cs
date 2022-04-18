ac312e966c77bb61fd50a47320e9bda3a133e24b
sssxrf <100052475+sssxrf@users.noreply.github.com>
2022-04-18 15:36:24 -0400

    Merge branch 'main' of https://github.com/cczw123/airplay-prototype

--- Assets/Scenes/real/PlayerControllerScript.cs
+++ Assets/Scenes/real/PlayerControllerScript.cs
@@ -17,41 +17,48 @@ public class PlayerControllerScript: MonoBehaviour
 	public Camera cam;
 	int port; //3
 	coordinate myObject;
-	bool local = false;
+	public bool local = false;
+	public float distance;
+	Vector2 position;
 
-	//public GameObject Player; //4
-	//AudioSource jumpSound; //5
-	//bool jump; //6
+    //public GameObject Player; //4
+    //AudioSource jumpSound; //5
+    //bool jump; //6
 
-	// 2. Initialize variables
-	void Start () 
+    // 2. Initialize variables
+    private void Awake()
+    {
+		distance = Vector2.Distance(dingwei.transform.GetChild(0).position,dingwei.transform.GetChild(1).position);
+		position = dingwei.transform.GetChild(1).position;
+
+	}
+    void Start () 
 	{
 		port = 5065; //1 
-		//jump = false; //2 
-		//jumpSound = gameObject.GetComponent<AudioSource>(); //3
+					 //jump = false; //2 
+					 //jumpSound = gameObject.GetComponent<AudioSource>(); //3
 
-		InitUDP(); //4
+		StartCoroutine(ReceiveData());//4
 	}
 
 	// 3. InitUDP
-	private void InitUDP()
-	{
-		print ("UDP Initialized");
+	//private void InitUDP()
+	//{
+	//	print ("UDP Initialized");
 
-		receiveThread = new Thread (new ThreadStart(ReceiveData)); //1 
-		receiveThread.IsBackground = true; //2
-		receiveThread.Start (); //3
+	//	receiveThread = new Thread (new ThreadStart(ReceiveData)); //1 
+	//	receiveThread.IsBackground = true; //2
+	//	receiveThread.Start (); //3
 
-	}
+	//}
 
 	// 4. Receive Data
-	private void ReceiveData()
+	IEnumerator  ReceiveData()
 	{
 		client = new UdpClient (port); //1
 		while (true) //2
 		{
-			try
-			{
+
 				IPEndPoint anyIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), port); //3
 				byte[] data = client.Receive(ref anyIP); //4
 				string text = Encoding.UTF8.GetString(data); //5
@@ -62,40 +69,37 @@ public class PlayerControllerScript: MonoBehaviour
                 }
                 else if(local)
                 {
-					myObject = JsonUtility.FromJson<coordinate>(text);
-					myObject.Convert();
+					coordinate myObject1 = JsonUtility.FromJson<coordinate>(text);
+					print(">> " + myObject1.x0 + " " + myObject1.y0 + " " + myObject1.nums);
+					myObject = myObject1.Convert(distance,position);
 					print(">> " + myObject.x0 + " " + myObject.y0 + " " + myObject.nums);
-				}
-				
-				
-				//Vector2 position = new Vector2(myObject.x , myObject.y);
-				//var newPlayer = Instantiate(player);
-				//newPlayer.transform.position = position;
-				
 
-			} catch(Exception e)
-			{
-				print (e.ToString()); //7
 			}
+
+
+			//Vector2 position = new Vector2(myObject.x , myObject.y);
+			//var newPlayer = Instantiate(player);
+			//newPlayer.transform.position = position;
+
+
+			yield return new WaitForEndOfFrame();
 		}
 	}
-
-
 	// 6. Check for variable value, and make the Player Jump!
 	void Update () 
 	{
         if (myObject != null)
         {
 			player.transform.position = new Vector2(myObject.x0, myObject.y0);
-			if (player.transform.position.x < -0.89f)
-			{
-				player.transform.Translate(Vector2.left);
-			}
+			//if (player.transform.position.x < -0.89f)
+			//{
+			//	player.transform.Translate(Vector2.left);
+			//}
 			player2.transform.position = new Vector2(myObject.x1, myObject.y1);
-			if (player2.transform.position.x < -0.89f)
-			{
-				player2.transform.Translate(Vector2.left);
-			}
+			//if (player2.transform.position.x < -0.89f)
+			//{
+			//	player2.transform.Translate(Vector2.left);
+			//}
 		}
         if (local)
         {
@@ -113,19 +117,21 @@ public class coordinate
 	public float y0;
 	public float x1;
 	public float y1;
-	public float x2;
-	public float y2;
 
-	public void Convert()
+    coordinate(int nums, float x0, float y0, float x1, float y1)
     {
+		this.nums = nums;
+		this.x0 = x0;
+		this.y0 = y0;
+		this.x1 = x1;
+		this.y1 = y1;
+    }
+	
 
-		x0 = 18.0f / 960 * x0 - 9;
-		x1 = 18.0f / 960 * x1 - 9;
-		x2 = 18.0f / 960 * x2 - 9;
-		y0 = 10.0f / 540 * y0 - 5;
-		y0 *= -1;
-		y1 = 10.0f / 540 * y1 - 5;
-		y1 *= -1;
-		y2 = 10.0f / 540 * y2 - 5;
+	public coordinate Convert(float distance, Vector2 point2)
+    {
+		coordinate newcord = new coordinate(2, x0 * distance + point2.x, y0 * distance + point2.y, x1 * distance + point2.x, y1 * distance + point2.y);
+		return newcord;
+
 	}
 }
